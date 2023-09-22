5e386c1abd0d4bab3d20c9048850643b81f3b74c
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2023-09-22 11:38:22 +0530

    Time: 2 ms (27.41%) | Memory: 40.5 MB (38.90%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.equals("")){
+            return true;
+        }
+        for(int i=0,j=0; j<t.length(); j++){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+            }
+            if(i == s.length()){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
