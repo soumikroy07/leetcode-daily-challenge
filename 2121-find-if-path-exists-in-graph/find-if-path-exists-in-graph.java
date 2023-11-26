35f1f72a2ae02b2d1cb04b5568fde9cdc649f65c
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2023-11-26 12:22:48 +0530

    Time: 48 ms (80.08%) | Memory: 122 MB (83.02%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if(source == destination){
+            return true;
+        }
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=0; i<n; i++){
             adj.add(new ArrayList<Integer>());
@@ -10,7 +13,8 @@ class Solution {
             adj.get(edge[1]).add(edge[0]);
         }
 
-        return dfs(source, adj, new boolean[n], destination);
+        // return dfs(source, adj, new boolean[n], destination);
+        return bfs(source, adj, new boolean[n], destination);
     }
 
     boolean dfs(int s, List<List<Integer>> adj, boolean visited[], int d){
@@ -26,6 +30,26 @@ class Solution {
             if(!visited[nei]){
                 if(dfs(nei, adj, visited, d)){
                     return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean bfs(int s, List<List<Integer>> adj, boolean visited[], int d){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(s);
+        visited[s] = true;
+
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            for(int nei: adj.get(curr)){
+                if(nei == d){
+                    return true;
+                }
+                if(!visited[nei]){
+                    q.add(nei);
+                    visited[nei] = true;
                 }
             }
         }
