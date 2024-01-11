f855c01dd730267b4342352593abd16814943217
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2024-01-11 11:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (39.17%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -21,21 +21,21 @@ class Solution {
     }
 
     int[] helper(TreeNode root){
-        if(root == null) return new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE};
+        if(root == null){
+            return new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE};
+        }
         if(root.left == null && root.right == null){
             return new int[]{root.val, root.val};
         }
-
-        int[] left = helper(root.left);
-        int[] right = helper(root.right);
+        int left[] = helper(root.left);
+        int right[] = helper(root.right);
 
         int min = Math.min(left[0], right[0]);
         int max = Math.max(left[1], right[1]);
 
-        diff = Math.max(diff, Math.max(Math.abs(root.val - min), Math.abs(root.val - max)));
+        diff = Math.max(diff, Math.max(Math.abs(root.val - min), Math.abs(max - root.val)));
         min = Math.min(min, root.val);
         max = Math.max(max, root.val);
         return new int[]{min, max};
     }
-
 }
