648d8ac2ca1c77fe9e7e131b9f1d9be97969ad26
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2024-02-03 16:26:30 +0530

    Time: 6 ms (93.17%) | Memory: 41.7 MB (77.66%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,80 +1,25 @@
-// Tabulation
-
 class Solution {
+    int dp[];
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        int n = arr.length;
-        int dp[] = new int[n+1];
-        
-        for(int i=n-1; i>=0; i--){
-            int len = 0, ans = 0, max = Integer.MIN_VALUE;
-            for(int j=i; j<Math.min(n, i+k); j++){
-                len++;
-                max = Math.max(max, arr[j]);
-                int sum = (len * max) + dp[j+1];
-                ans = Math.max(ans, sum);
-            }
-            dp[i] = ans;
+        dp = new int[arr.length+1];
+        return help(arr, k, 0);
+    }
+
+    int help(int arr[], int k, int idx){
+        if(idx >= arr.length){
+            return 0;
         }
-        
-        return dp[0];
-        
-    }
-}
-
-
-/*
-
-memorization TC : O(n) * O(k)
-
-class Solution {
-    public int maxSumAfterPartitioning(int[] arr, int k) {
-        int dp[] = new int[arr.length];
-        return helper(arr, k, 0, dp);
-    }
-    
-    int helper(int arr[], int k, int idx, int dp[]){
-        if(idx >= arr.length) return 0;
-        
-        if(dp[idx] != 0) return dp[idx];
-        
-        int ans = 0, len = 0, max = Integer.MIN_VALUE;
-        
-        for(int j=idx; j<Math.min(arr.length, idx+k); j++){
+        if(dp[idx] != 0){
+            return dp[idx];
+        }
+        int ans = 0, max = 0, len = 0;;
+        for(int i=idx; i<Math.min(idx+k, arr.length); i++){
             len++;
-            max = Math.max(max, arr[j]);
-            int sum = (max * len) + helper(arr, k, j+1, dp);
+            max = Math.max(max, arr[i]);
+            int sum = (max * len) + help(arr, k, i+1);
             ans = Math.max(sum, ans);
         }
-        
+
         return dp[idx] = ans;
     }
 }
-
-
-
-
-
-resursive approch
-
-class Solution {
-    public int maxSumAfterPartitioning(int[] arr, int k) {
-        return helper(arr, k, 0);
-    }
-    
-    int helper(int arr[], int k, int idx){
-        if(idx >= arr.length) return 0;
-        
-        int ans = 0, len = 0, max = Integer.MIN_VALUE;
-        
-        for(int j=idx; j<Math.min(arr.length, idx+k); j++){
-            len++;
-            max = Math.max(max, arr[j]);
-            int sum = (max * len) + helper(arr, k, j+1);
-            ans = Math.max(sum, ans);
-        }
-        
-        return ans;
-    }
-}
-
-*/
