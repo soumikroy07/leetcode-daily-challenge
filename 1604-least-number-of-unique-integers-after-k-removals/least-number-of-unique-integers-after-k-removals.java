70494126650675f2bf0819e263cb68cc7e2fc42b
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2024-02-16 10:00:21 +0530

    Time: 53 ms (47.69%) | Memory: 58 MB (55.62%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -5,21 +5,6 @@ class Solution {
             map.put(a, map.getOrDefault(a, 0) + 1);
         }
 
-        // int freq[][] = new int[map.size()][2];
-
-        // int i=0;
-        // for(Map.Entry<Integer, Integer> m: map.entrySet()){
-        //     freq[i][0] = m.getKey();
-        //     freq[i][1] = m.getValue();
-        //     i++;
-        // }
-
-        // Arrays.sort(freq, (a, b) -> a[1] - b[1]);
-
-        // for(i=0; i<freq.length; i++){
-        //     if()
-        // }
-
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(Map.Entry<Integer, Integer> m: map.entrySet()){
             pq.add(m.getValue());
