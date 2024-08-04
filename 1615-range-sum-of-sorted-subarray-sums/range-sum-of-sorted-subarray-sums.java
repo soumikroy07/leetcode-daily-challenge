4ee17a80a9e10abd4eb4c47d2f03bbe75a3565bb
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2024-08-04 09:54:34 +0530

    Time: 65 ms (72.52%) | Memory: 46.6 MB (43.69%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -0,0 +1,42 @@
+/*
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int sums[] = new int[n * (n+1) / 2];
+        int idx = 0;
+        for(int i=0; i<n; i++){
+            int sum = 0;
+            for(int j=i; j<n; j++){
+                sum += nums[j];
+                sums[idx++] = sum;
+            }
+        }
+
+        Arrays.sort(sums);
+        int ans = 0;
+        for(int i=left-1; i<right; i++){
+            ans += sums[i];
+        }
+        return ans;
+    }
+}
+*/
+
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int[] arr = new int[n * (n + 1) / 2];
+        for (int i = 0, k = 0; i < n; ++i) {
+            int s = 0;
+            for (int j = i; j < n; ++j) {
+                s += nums[j];
+                arr[k++] = s;
+            }
+        }
+        Arrays.sort(arr);
+        int ans = 0;
+        final int mod = (int) 1e9 + 7;
+        for (int i = left - 1; i < right; ++i) {
+            ans = (ans + arr[i]) % mod;
+        }
+        return ans;
+    }
+}
