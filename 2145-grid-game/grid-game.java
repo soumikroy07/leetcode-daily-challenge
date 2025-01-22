2eb2f5782bf226a5440dc0a8ca762a8fb14bd9de
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-01-22 09:30:46 +0530

    Time: 5 ms (57.73%) | Memory: 58.1 MB (14.43%) - LeetSync

---
+++ 2145-grid-game/grid-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long gridGame(int[][] grid) {
+        long top = 0, bottom = 0;
+        for(int i=0; i<grid[0].length; i++){
+            top += grid[0][i];
+            bottom += grid[1][i];
+        }
+
+        long currBottom = 0, currTop = top - grid[0][0];
+        long robo2 = top-grid[0][0];
+        for(int i=1; i<grid[0].length; i++){
+            currBottom += grid[1][i-1];
+            currTop -= grid[0][i];
+            robo2 = Math.min(robo2, Math.max(currBottom, currTop));
+        }
+
+        return robo2;
+    }
+}
