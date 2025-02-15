c3c4ceabe6a210634511323f0515834809c091e4
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-02-15 10:55:22 +0530

    Time: 148 ms (31.66%) | Memory: 45.1 MB (5.33%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,36 @@
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public int punishmentNumber(int n) {
+        calculateInRange(n);
+        int ans = 0;
+        for(int val : list){
+            ans += (val * val);
+        }
+        return ans;
+    }
+
+    void calculateInRange(int n){
+        list.add(1);
+        for(int i=2; i<=n ;i++){
+            int num = i * i;
+            if(isValid(Integer.toString(num), i)){
+                list.add(i);
+            }
+        }
+    }
+
+    boolean isValid(String num, int sum){
+        if(sum < 0){
+            return false;
+        }
+        if(sum == 0 && num.isEmpty()){
+            return true;
+        }
+        boolean ans = false;
+        for(int i=0; i<num.length(); i++){
+            String curr = num.substring(0, i+1);
+            ans |= isValid(num.substring(i+1), sum-Integer.parseInt(curr));
+        }
+        return ans;
+    }
+}
