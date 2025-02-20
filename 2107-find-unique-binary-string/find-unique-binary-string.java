eacb85c2895ed4978f608d0b0b47fb248b233714
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-02-20 23:30:09 +0530

    Time: 1 ms (67.36%) | Memory: 41.7 MB (57.18%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,51 +1,31 @@
 class Solution {
+    Set<String> set;
+    String ans = "";
     public String findDifferentBinaryString(String[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int size = 1, num = 2;
-
-        while(size <= 16){
-            map.put(size, num);
-            size++;
-            num = num * 2;
+        set = new HashSet<>();
+        for(String num: nums){
+            set.add(num);
         }
-
-        int n = nums.length;
-        Set<String> set = new HashSet<>();
-        for(String s : nums){
-            set.add(s);
-        }
-
-        int start = map.get(n)-1;
-
-        while(start >= 0){
-            String binary = getBinary(start, n);
-            if(!set.contains(binary)){
-                return binary;
-            }
-            start--;
-        }
-
-        return "";
+        StringBuilder sb = new StringBuilder();
+        help(sb, nums[0].length());
+        return ans;
     }
 
-    String getBinary(int num, int size){
-        if(num == 0){
-            return "0";
-        }
-        Stack<Integer> st = new Stack<>();
-        while(num > 0){
-            st.push(num % 2);
-            num = num / 2;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        while(st.size() < size){
-            st.push(0);
-        }
-        while(!st.isEmpty()){
-            sb.append(st.pop());
+    boolean help(StringBuilder sb, int n){
+        if(sb.length() == n){
+            if(!set.contains(sb.toString())){
+                ans = sb.toString();
+                return true;
+            }
+            return false;
         }
 
-        return sb.toString();
+        sb.append('0');
+        if(help(sb, n)) return true;
+        sb.setLength(sb.length() - 1);
+        sb.append('1');
+        if(help(sb, n)) return true;
+        sb.setLength(sb.length() - 1);
+        return false;
     }
 }
