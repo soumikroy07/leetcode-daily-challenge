e33c3243105855b177ed3a48b1321cf4cf935c72
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-08-10 01:03:02 +0530

    Time: 63 ms (14.42%) | Memory: 60.3 MB (96.03%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -1,28 +1,38 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int res = -1;
         int sum = 0;
-        for(int n : nums){
-            sum += n;
+
+        for(int num: nums){
+            sum += num;
         }
+
         if(sum == x){
             return nums.length;
         }
-        int s = 0;
-        int sidx = 0;
 
-        for(int i = 0; i < nums.length; i++){
-            s += nums[i];
-            while(sidx < i && sum - s < x){
-                s -= nums[sidx++];
+        int len = longestLengthOfSubArraySumEqualsK(nums, sum - x);
+
+        return len == -1 ? len : nums.length - len; 
+    }
+
+    int longestLengthOfSubArraySumEqualsK(int nums[], int k){
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = -1;
+        map.put(0, -1);
+
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            if(map.containsKey(sum - k)){
+                int j = map.get(sum - k);
+                ans = Math.max(ans, i - j);
             }
-            if(sum - s == x){
-                res = Math.max(res, i - sidx + 1);
+
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
             }
         }
-        if(res == -1){
-            return res;
-        }
-        return nums.length - res;
+
+        return ans;
     }
 }
