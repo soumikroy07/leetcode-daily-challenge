900718ad2ecc93d2c927a6c8baa0f03136460a6e
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-08-18 23:29:04 +0530

    Time: 7 ms (73.88%) | Memory: 61.8 MB (48.88%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.java
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] findFarmland(int[][] land) {
-        List<List<Integer>> list = new ArrayList<>();
         int m = land.length, n = land[0].length;
+        List<int[]> list = new ArrayList<>();
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(land[i][j] == 1){
@@ -11,16 +11,18 @@ class Solution {
                             land[r][c] = 0;
                         }
                     }
-                    list.add(List.of(i, j, r-1, c-1));
+                    list.add(new int[]{i, j, r-1, c-1});
                 }
             }
         }
+
         int ans[][] = new int[list.size()][4];
         for(int i=0; i<ans.length; i++){
             for(int j=0; j<ans[0].length; j++){
-                ans[i][j] = list.get(i).get(j);
+                ans[i][j]=list.get(i)[j];
             }
         }
+
         return ans;
     }
 }
