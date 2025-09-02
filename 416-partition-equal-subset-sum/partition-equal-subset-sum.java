c81c5fd84852e46e4bab2c394b860b87d8250c74
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-09-02 10:19:56 +0530

    Time: 84 ms (28.88%) | Memory: 53.3 MB (42.66%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,49 +1,45 @@
 class Solution {
+    boolean dp[][];
+    boolean visited[][];
     public boolean canPartition(int[] nums) {
-        int total = 0;
-        for(int i=0; i<nums.length; i++){
-            total += nums[i];
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
         }
-        if(total % 2 != 0) return false;
-        
-        int sum = total/2;
-        boolean dp[][] = new boolean[nums.length+1][sum+1];
-        
-        for(int i=0; i<dp.length; i++){
-            for(int j=0; j<dp[0].length; j++){
-                if(i==0 && j ==0){
-                    dp[i][j] = true;
-                }else if(i==0){
-                    dp[i][j] = false;
-                }else if(j == 0){
-                    dp[i][j] = true;
-                }else{
-                    if(dp[i-1][j]){
-                        dp[i][j] = true;
-                    }else{
-                        int val = nums[i-1];
-                        if(j-val >= 0){
-                            dp[i][j] = dp[i-1][j-val];
-                        }
-                    }
-                }
-            }
+
+        if (sum % 2 == 1) {
+            return false;
         }
-        
-        return dp[nums.length][sum];
-        
-        // return fun(nums, 0, sum);
+
+        sum = sum / 2;
+        dp = new boolean[nums.length+1][sum+1];
+        visited = new boolean[nums.length+1][sum+1];
+        return help(nums, 0, sum);
     }
-    
-    boolean fun(int nums[], int i, int sum){
-        if(sum == 0){
+
+    boolean help(int nums[], int i, int sum) {
+        if (sum == 0) {
             return true;
         }
-        boolean ans1=false, ans2=false;
-        if(i < nums.length){
-            ans1 = fun(nums, i+1, sum-nums[i]);
-            ans2 = fun(nums, i+1, sum);
+
+        if (sum < 0 || i >= nums.length) {
+            return false;
         }
+
+        // if(visited[i][sum]){
+        //     return dp[i][sum];
+        // }
+        if(dp[i][sum]){
+            return false;
+        }
+
+        boolean ans1 = help(nums, i + 1, sum - nums[i]);
+        boolean ans2 = help(nums, i + 1, sum);
+
+        dp[i][sum] = true;
+
+        // visited[i][sum] = true;
+        // return dp[i][sum] = ans1 || ans2;
         return ans1 || ans2;
     }
 }
