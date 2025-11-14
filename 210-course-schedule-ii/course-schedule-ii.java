a320ba3c26b554b4fb8f6d2441df10dae29e5601
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-11-14 10:13:10 +0530

    Time: 4 ms (93.83%) | Memory: 46.5 MB (21.79%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,11 +1,12 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
+        
+        for(int i=0; i<numCourses; i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int pre[]: prerequisites){
+        for(int[] pre: prerequisites){
             adj.get(pre[0]).add(pre[1]);
         }
 
@@ -13,38 +14,41 @@ class Solution {
             return new int[]{};
         }
 
-        boolean visited[] = new boolean[numCourses];
         Stack<Integer> st = new Stack<>();
+        boolean visited[] = new boolean[numCourses];
 
-        for (int i=0; i<numCourses; i++){
+        for(int i=0; i<numCourses; i++){
             if(!visited[i]){
-                dfs(adj, i, visited, st);
+                topo(adj, i, visited, st);
             }
         }
 
-        int ans[] = new int[st.size()];
+        int i = numCourses - 1;
+        int ans[] = new int[numCourses];
 
-        int i=st.size()-1;
-        while (!st.isEmpty()){
+        while(!st.isEmpty()){
             ans[i--] = st.pop();
         }
+
         return ans;
     }
 
-    private static void dfs(List<List<Integer>> adj, int node, boolean[] visited, Stack<Integer> st) {
+    void topo(List<List<Integer>> adj, int node, boolean visited[], Stack<Integer> st){
         visited[node] = true;
-        for (int nei: adj.get(node)){
+        for(int nei: adj.get(node)){
             if(!visited[nei]){
-                dfs(adj, nei, visited, st);
+                topo(adj, nei, visited, st);
             }
         }
+
         st.push(node);
     }
 
-    private static boolean hasCycle(List<List<Integer>> adj, int numCourses) {
-        boolean visited[] = new boolean[numCourses];
-        boolean recStack[] = new boolean[numCourses];
-        for (int i=0; i<numCourses; i++){
+    boolean hasCycle(List<List<Integer>> adj, int n){
+        boolean visited[] = new boolean[n];
+        boolean recStack[] = new boolean[n];
+
+        for(int i=0; i<n; i++){
             if(!visited[i]){
                 if(dfs(adj, i, visited, recStack)){
                     return true;
@@ -55,21 +59,22 @@ class Solution {
         return false;
     }
 
-    private static boolean dfs(List<List<Integer>> adj, int node, boolean[] visited, boolean[] recStack) {
+    boolean dfs(List<List<Integer>> adj, int node, boolean visited[], boolean recStack[]){
         visited[node] = true;
         recStack[node] = true;
 
-        for (int nei: adj.get(node)){
+        for(int nei: adj.get(node)){
             if(!visited[nei]){
                 if(dfs(adj, nei, visited, recStack)){
                     return true;
                 }
-            } else if (recStack[nei]) {
+            }else if(recStack[nei]){
                 return true;
             }
         }
 
         recStack[node] = false;
+
         return false;
     }
 }
