ecf07130e5b4b2d1ce6edac88a0cb6cc79da5bc2
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-11-15 12:35:03 +0530

    Time: 6 ms (83.17%) | Memory: 46.6 MB (12.01%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,59 +1,40 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        List<List<int[]>> adj = new ArrayList<>();
+
         for(int i=0; i<n; i++){
-            adj.add(new ArrayList<Pair>());
+            adj.add(new ArrayList<>());
         }
-        
-        for(int i=0; i<flights.length; i++){
-            adj.get(flights[i][0]).add(new Pair(flights[i][1], flights[i][2]));
+
+        for(int[] flight: flights){
+            int s = flight[0], d = flight[1], price = flight[2];
+            adj.get(s).add(new int[]{d, price});
         }
-        
-        int distance[] = new int[n];
-        for(int i=0; i<n; i++){
-            distance[i] = Integer.MAX_VALUE;
-        }
-        distance[src] = 0;
-        
-        Queue<Triple> q = new LinkedList<>();
-        
-        q.add(new Triple(0, src, 0));
-        
-        while(!q.isEmpty()){
-            Triple curr = q.poll();
-            int stops = curr.stops;
-            int node = curr.node;
-            int cost = curr.cost;
-            
-            if(stops > k) continue;
-            for(Pair neighbour : adj.get(node)){
-                int destination = neighbour.des;
-                int currCost = neighbour.cost;
-                
-                if(cost+currCost < distance[destination] && stops <= k){
-                    distance[destination] = cost + currCost;
-                    q.add(new Triple(stops+1, destination, cost+currCost));
+
+        int cost[] = new int[n];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        cost[src] = 0;
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{src, 0, 0});
+
+        while (!q.isEmpty()){
+            int[] curr = q.poll();
+            int curr_src = curr[0];
+            int curr_cost = curr[1];
+            int curr_stops = curr[2];
+
+            if(curr_stops > k) continue;
+            for (int[] nei: adj.get(curr_src)){
+                int next_cost = nei[1];
+                int next_dest = nei[0];
+                if(cost[next_dest] > curr_cost + next_cost && curr_stops <= k){
+                    cost[next_dest] = curr_cost + next_cost;
+                    q.add(new int[]{next_dest, curr_cost + next_cost, curr_stops + 1});
                 }
             }
         }
-        
-        return distance[dst] == Integer.MAX_VALUE ? -1 : distance[dst];
-    }
-}
 
-class Pair{
-    int des, cost;
-    Pair(int d, int c){
-        des = d;
-        cost = c;
-    }
-}
-
-class Triple{
-    int stops, node, cost;
-    Triple(int s, int n, int c){
-        stops = s;
-        node = n;
-        cost = c;
+        return cost[dst] == Integer.MAX_VALUE ? -1 : cost[dst];
     }
 }
