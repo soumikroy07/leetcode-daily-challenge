9eb374af26600c912fda6260f90af47f8b6564f6
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-11-19 11:00:46 +0530

    Time: 15 ms (7.55%) | Memory: 56.2 MB (5.15%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,53 +1,27 @@
 class Solution {
+    Integer dp[][][];
     public int maxProfit(int k, int[] prices) {
-        int dp[] = new int[2*k];
-        for(int i=0; i<2*k; i++){
-            if(i % 2 == 0){
-                dp[i] = Integer.MIN_VALUE;
-            }else{
-                dp[i] = 0;
-            }
-        }
-
-        for(int i=0; i<prices.length; i++){
-            for(int j=0; j<2*k; j++){
-                if(j == 0){
-                    dp[j] = Math.max(dp[j], -prices[i]);
-                }else if(j % 2 == 0){
-                    dp[j] = Math.max(dp[j], dp[j-1] - prices[i]);
-                }else{
-                    dp[j] = Math.max(dp[j], dp[j-1] + prices[i]);
-                }
-            }
-        }
-
-        return dp[2*k - 1];
-    }
-}
-
-/*
-
-class Solution {
-    public int maxProfit(int k, int[] prices) {
-        return help(prices, 0, k, 0, 0);
+        dp = new Integer[prices.length + 1][k+1][2];
+        return help(prices, 0, k, 0);
     }
 
-    int help(int prices[], int i, int k, int buy, int price){
-        if(prices.length <= i || k < 0){
+    int help(int prices[], int i, int k, int buy){
+        if(prices.length <= i || k == 0){
             return 0;
+        }
+        if(dp[i][k][buy] != null){
+            return dp[i][k][buy];
         }
         int ans1 = 0, ans2 = 0, ans3 = 0, ans4 = 0;
         if(buy == 0){
-            ans1 = help(prices, i+1, k, 1, prices[i]);
-            ans2 = help(prices, i+1, k, 0, price);
+            ans1 = -prices[i] + help(prices, i+1, k, 1);
+            ans2 = help(prices, i+1, k, 0);
         }
         else{
-            ans3 = (prices[i] - price) + help(prices, i+1, k-1, 0, 0);
-            ans4 = help(prices, i+1, k, 1, price);
+            ans3 = prices[i] + help(prices, i+1, k-1, 0);
+            ans4 = help(prices, i+1, k, 1);
         }
 
-        return Math.max(ans1, Math.max(ans2, Math.max(ans3, ans4)));
+        return dp[i][k][buy] = Math.max(ans1, Math.max(ans2, Math.max(ans3, ans4)));
     }
 }
-
-*/
