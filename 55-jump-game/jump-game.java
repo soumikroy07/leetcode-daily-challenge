9698040693251f22d800ee3fc4548b646bb578a4
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-11-29 20:23:47 +0530

    Time: 2 ms (84.12%) | Memory: 47.8 MB (32.49%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length == 1) return true;
+        if(nums.length == 1){
+            return true;
+        }
 
-        int boundary = nums[0];
-        int i=0;
-        while(i < boundary){
+        int boundary = nums[0], i = 0 ;
+
+        while(i <= boundary){
+            boundary = Math.max(i + nums[i], boundary);
             i++;
-            boundary = Math.max(nums[i]+i, boundary);
             if(boundary >= nums.length-1){
                 return true;
             }
