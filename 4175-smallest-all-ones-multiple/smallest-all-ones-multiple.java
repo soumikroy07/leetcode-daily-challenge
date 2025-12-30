414f08aa1b7eff3f04027219ce561f8aef134a25
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2025-12-30 21:14:53 +0530

    Time: 117 ms (5.31%) | Memory: 89.8 MB (5.01%) - LeetSync

---
+++ 4175-smallest-all-ones-multiple/smallest-all-ones-multiple.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minAllOneMultiple(int k) {
+        int count = 1;
+        int num = 1;
+        Set<Integer> set = new HashSet<>();
+        while(num % k != 0){
+            int rem = num % k;
+            if(set.contains(rem)){
+                return -1;
+            }
+            set.add(rem);
+            num = rem * 10 + 1;
+            count++;
+        }
+        
+        return count;
+    }
+}
