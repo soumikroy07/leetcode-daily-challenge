f11db919da78074710135ac88c87dc8461fe737f
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2026-01-13 19:15:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (14.43%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,23 +1,65 @@
 class Solution {
-    public int search(int[] arr, int target) {
-        
-        int low = 0, high = arr.length - 1;
+    public int search(int[] nums, int target) {
+        if(nums.length == 1 && nums[0] != target){
+            return -1;
+        }else if(nums.length == 1 && nums[0] == target){
+            return 0;
+        }
+        int pivotIdx = searchPivot(nums);
+        int ans = -1;
+        if(pivotIdx == -1){
+            return ans;
+        }
+        if(target < nums[0]){
+            ans = searchIdx(nums, target, pivotIdx+1, nums.length-1);
+        }else{
+            ans = searchIdx(nums, target, 0, pivotIdx);
+        }
+        return ans;
+    }
+
+    private int searchIdx(int nums[], int target, int low, int high){
+        int ans = -1;
         while(low <= high){
-            int mid = low + (high - low)/2;
-            if(target == arr[mid]){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
                 return mid;
-            }else if(arr[low] <= arr[mid]){
-                if(arr[mid] >= target && arr[low] <= target){
-                    high = mid-1;
-                }else{
-                    low = mid+1;
-                }
+            }else if(nums[mid] > target){
+                high = mid-1;
             }else{
-                if(arr[mid] <= target && arr[high] >= target){
-                    low = mid+1;
-                }else{
-                    high = mid-1;
-                }
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+
+    int searchPivot(int arr[]){
+        int low = 0;
+        int high = arr.length - 1;
+
+        // If the array is not rotated
+        if (arr[low] <= arr[high]) {
+            return high;
+        }
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Check if mid+1 is the maximum element
+            if (mid < high && arr[mid + 1] < arr[mid]) 
+                return mid;
+            
+
+            // Check if mid is the maximum element
+            if (mid > low && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            // Decide whether to search in the left or right half
+            if (arr[high] > arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
