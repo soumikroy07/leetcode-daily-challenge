fcae96a309f7db264dde525dce27297c9d474a16
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2026-02-02 23:45:22 +0530

    Time: 1 ms (99.64%) | Memory: 43.8 MB (55.59%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,29 +1,35 @@
 class Solution {
-    public int myAtoi(String str) {
-        str = str.trim();
-        if (str == null || str.length() == 0)
-            return 0;//
-        
-        char firstChar = str.charAt(0);
-        int sign = 1, start = 0, len = str.length();
-        long sum = 0;
-        if (firstChar == '+') {
-            sign = 1;
-            start++;
-        } else if (firstChar == '-') {
-            sign = -1;
-            start++;
-        }
-        for (int i = start; i < len; i++) {
-            if (!Character.isDigit(str.charAt(i)))
-                return (int) sum * sign;
-            sum = sum * 10 + str.charAt(i) - '0';
-            if (sign == 1 && sum > Integer.MAX_VALUE)
-                return Integer.MAX_VALUE;
-            if (sign == -1 && (-1) * sum < Integer.MIN_VALUE)
-                return Integer.MIN_VALUE;
+    public int myAtoi(String s) {
+        s = s.trim();
+        if(s== null || s.length() == 0){
+            return 0;
         }
 
-        return (int) sum * sign;
+        int sign = 1, start = 0;
+        long sum = 0L;
+        if(s.charAt(0) == '-'){
+            sign = -1;
+            start++;
+        }else if(s.charAt(0) == '+'){
+            sign = 1;
+            start++;
+        }
+
+        for(int i=start; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(!Character.isDigit(c)){
+                return (int)sum * sign;
+            }
+            sum = sum  * 10 + (c - '0');
+            if(sign == 1 && sum > Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }
+
+            if(sign == -1 && (-1) * sum < Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
+        }
+
+        return (int)sum * sign;
     }
 }
