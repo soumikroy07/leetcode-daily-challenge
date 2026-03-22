8cb839173cc8633a1daeedcebdf86df45fcc21ac
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2026-03-22 11:41:02 +0530

    Time: 36 ms (29.49%) | Memory: 71.3 MB (63.73%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.java
+++ 931-maximum-frequency-stack/maximum-frequency-stack.java
@@ -2,37 +2,37 @@ class FreqStack {
     Map<Integer, Stack<Integer>> stack;
     Map<Integer, Integer> freq;
     int max;
-
     public FreqStack() {
         stack = new HashMap<>();
         freq = new HashMap<>();
         max = 0;
     }
-
+    
     public void push(int val) {
         freq.put(val, freq.getOrDefault(val, 0) + 1);
         int count = freq.get(val);
-        if (stack.containsKey(count)) {
+        if(stack.containsKey(count)){
             Stack st = stack.get(count);
             st.push(val);
             stack.put(count, st);
-        } else {
+        }else{
             Stack<Integer> st = new Stack<>();
             st.push(val);
             stack.put(count, st);
         }
         max = Math.max(max, count);
     }
-
+    
     public int pop() {
+        // if(max == 0){
+        //     return -1;
+        // }
+
+        int ans = -1;
         Stack<Integer> st = stack.get(max);
-        int ans = st.pop();
+        if(!st.isEmpty()) ans = st.pop();
+        if(st.isEmpty()) max--;
         freq.put(ans, freq.get(ans) - 1);
-
-        if (st.isEmpty()) {
-            max--;
-        }
-
         return ans;
     }
 }
