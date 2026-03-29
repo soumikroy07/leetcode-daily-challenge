b06d34be9f20f79a0cfdd587f7fec559fdfa85e8
SOUMIK ROY <85670300+soumikroy07@users.noreply.github.com>
2026-03-29 23:28:13 +0530

    Time: 105 ms (36.71%) | Memory: 46.8 MB (86.08%) - LeetSync

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.java
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.java
@@ -1,49 +1,4 @@
 class Solution {
-    public boolean differByOneChar(String word1, String word2) {
-        if (word1.length() != word2.length()) return false;
-        int diffCount = 0;
-        for (int i = 0; i < word1.length(); i++)
-            if (word1.charAt(i) != word2.charAt(i))
-                diffCount++;
-        return diffCount == 1;
-    }
-
-    public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
-        int n = groups.length;
-        int[] dp = new int[n];
-        int[] parent = new int[n];
-        Arrays.fill(dp, 1);
-        Arrays.fill(parent, -1);
-        int maxi = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (groups[i] != groups[j] &&
-                    differByOneChar(words[i], words[j]) &&
-                    dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    parent[i] = j;
-                }
-            }
-            if (dp[i] > maxi) maxi = dp[i];
-        }
-
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (dp[i] == maxi) {
-                while (i != -1) {
-                    result.add(words[i]);
-                    i = parent[i];
-                }
-                break;
-            }
-        }
-        Collections.reverse(result);
-        return result;
-    }
-}
-/*
-class Solution {
     public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
         List<String> ans = new ArrayList<>();
         // if(words.length == 1){
@@ -65,17 +20,20 @@ class Solution {
         }
 
         String prev = "";
+        int lastGroup = -1;
         for (int i=n-1; i>=0; i--){
             if(lis[i] == maxLength && prev.length() == 0){
                 prev = words[i];
                 ans.add(prev);
                 maxLength--;
+                lastGroup = groups[i];
             }
 
-            if(lis[i] == maxLength && isHammingDistance(prev, words[i])){
+            if(lis[i] == maxLength && isHammingDistance(prev, words[i]) && lastGroup != groups[i]){
                 maxLength--;
                 prev = words[i];
                 ans.add(0, prev);
+                lastGroup = groups[i];
             }
         }
         return ans;
@@ -85,7 +43,27 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        
+        /*
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c: t.toCharArray()){
+            if(map.containsKey(c)){
+                map.put(c, map.get(c) - 1);
+                if(map.get(c) == 0){
+                    map.remove(c);
+                }
+            }
+        }
+
+        if(map.size() > 1){
+            return false;
+        }
+
+        return map.values().stream().findFirst().orElse(2) == 1;
+        */
         int count = 0;
         for (int i=0; i<s.length(); i++){
             if(s.charAt(i) != t.charAt(i)){
@@ -96,5 +74,3 @@ class Solution {
         return  count <= 1;
     }
 }
-
-*/
